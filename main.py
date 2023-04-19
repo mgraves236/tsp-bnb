dced340050d7339bb4c0e22dee50a8135b92cebe
mgraves236 <m.graves236@gmail.com>
2023-04-19 16:07:11 +0200

    .

--- main.py
+++ main.py
@@ -20,25 +20,58 @@ def generate_graph(size: int):
     return matrix
 
 
-# lower bound calculated as >= sum_(v in V) (sum of the cost of the two
-# least cost edges adjacent to v)
-def lower_bound(matrix: array, i):
+def least_cost_edges(matrix: array, i : int):
     # node # least cost edges # total cost
     least_cost = {'min1': maxsize, 'min2': maxsize}
     # find first minimum
     for j in range(0, size):
-        if i == j: continue
-        elif matrix[i][j] <  least_cost['min1'] and i != j:
+        if i == j:
+            continue
+        elif matrix[i][j] < least_cost['min1'] and i != j:
             least_cost['min1'] = matrix[i][j]
         elif least_cost['min2'] > matrix[i][j] != least_cost['min1'] and i != j:
             least_cost['min2'] = matrix[i][j]
+    return least_cost
 
-    print(least_cost)
+
+# lower bound calculated as >= sum_(v in V) (sum of the cost of the two
+# least cost edges adjacent to v)
+
+def lower_bound(adj : array, i: int, j: int, level: int):
+    least_i = least_cost_edges(adj, i)
+    least_j = least_cost_edges(adj, j)
+
+    if level == 1:
+        return (least_i['min1'] + least_j['min1']) * 1/2 + adj[i][j]
+
+    if level > 1:
+        return (least_i['min2'] + least_j['min1']) * 1/2 + adj[i][j]
 
 
 def tsp(adj: array):
     # tour always starts at node 0
-    lower_bound(adj, 0)
+    curr_lower_bound = 0
+    upper_bound = maxsize
+    visited = [False] * size
+    curr_path = []
+    level = 0 # keep track of depth of the tree
+
+    least = []
+    sum = 0
+    # always start from node 0
+    for i in range(0, size):
+        least = least_cost_edges(adj, i)
+        print(least['min1'] + least['min2'])
+        sum += least['min1'] + least['min2']
+    sum = sum / 2
+    curr_lower_bound = sum
+    visited[0] = True
+    curr_path.append(0)
+    # lower_bound(adj, 0, 1, 1)
+
+    if level == size:
+        return
+
 
 
 if __name__ == "__main__":
